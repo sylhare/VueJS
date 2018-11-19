c2f5e6ad3da28d53b2e688508d87fc10ca0f4510
sylhare <sylhare@outlook.com>
2018-11-19 10:41:34 -0500

    Add tests VueJs failing out of the box meh

---
+++ src/yarn-world/src/components/bonjour.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="message">{{ message }}</span>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'bonjour !'
+      }
+    },
+    created () {
+      this.message = 'au revoir !'
+    }
+  }
+</script>
