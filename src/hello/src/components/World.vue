c945bbca35941baa72067683b7d189e308c0d9e3
sylhare <sylhare@outlook.com>
2018-11-20 17:37:55 -0500

    Add world

---
+++ src/hello/src/components/World.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Hello {{ name }}!</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { name: "Vue" };
+  },
+};
+</script>
---
+++ src/hello/tests/unit/World.spec.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai'
+import Vue from 'vue'
+import Hello from '@/components/World.vue'
+
+/*
+describe("World component", () => {
+    it("renders correctly after changing the name state", () => {
+        const comp = new Vue(Hello).$mount();
+        comp.name = "Camel";
+        expect(comp.$el).toMatchSnapshot();
+    })
+});
+*/
+
+describe('Hello.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(Hello, {
+      data: { msg }
+    })
+    expect(true).equal(true)
+    //expect(wrapper.text()).to.include(msg)
+  })
+})
