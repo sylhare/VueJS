180c8fd5aa0f025ac5220f56a6b4c75029460a14
sylhare <sylhare@outlook.com>
2018-11-25 16:21:32 -0500

    Update http example

--- src/vuejs-jest-example/src/components/ExampleHttp.vue
+++ src/vuejs-jest-example/src/components/ExampleHttp.vue
@@ -4,7 +4,7 @@
         <input type="text" v-model="input.firstname" placeholder="First Name" />
         <input type="text" v-model="input.lastname" placeholder="Last Name" />
         <button v-on:click="sendData()">Send</button>
-        
+
         <br />
         <p> Looking for "{{ fullname }}" from {{ httpurl }} </p>
         <textarea>{{ response }}</textarea>
@@ -20,55 +20,62 @@
 </style>
 
 <script>
-    export default {
-        name: 'example',
-        data() {
-            return {
-                ip: "",
-                httpurl: this.$httpbin,
-                input: {
-                    firstname: "",
-                    lastname: "",
-                },
-                response: ""
-            }
-        },
-        created() {
-            //console.log(this)
-            console.log(this.$httpbin)
-            this.$http.get('https://jsonplaceholder.typicode.com/users').then(function(result) {
-                //this.response = result.data
-            })
-        },
-        
-        mounted() {
-            this.$http.get("https://httpbin.org/ip").then(result => {
-                this.ip = result.body.origin;
-            }, error => {
-                console.error(error);
-            });
-        },
+export default {
+  name: 'example',
+  data() {
+    return {
+      ip: '',
+      httpurl: this.$httpbin,
+      input: {
+        firstname: '',
+        lastname: '',
+      },
+      response: '',
+    };
+  },
+  created() {
+    // console.log(this)
+    //console.log(this.$httpbin);
+    var auth = 'Basic ' + new Buffer('username' + ':' + 'password').toString('base64');
+      
+    this.$http.get('https://jsonplaceholder.typicode.com/users', {}, {
+      headers: {
+        'Authorization': auth
+      }
+    }).then((result) => {
+      console.log(result);
+      console.log(auth);
+    });  
+  },
 
-        computed: {
-            fullname: function() {
-                return this.input.firstname + ' ' + this.input.lastname
-            }
-        },
+  mounted() {
+    this.$http.get('https://httpbin.org/ip').then((result) => {
+      this.ip = result.body.origin;
+    }, (error) => {
+      console.error(error);
+    });
+  },
 
-        methods: {
-            sendData() {
-                console.log("sending")
-                this.$http.post("https://httpbin.org/post", this.input, {
-                    headers: {
-                        "content-type": "application/json"
-                    }
-                }).then(result => {
-                    this.response = result.data;
-                }, error => {
-                    console.error(error);
-                });
-            }
-        }
-    }
+  computed: {
+    fullname() {
+      return `${this.input.firstname} ${this.input.lastname}`;
+    },
+  },
+
+  methods: {
+    sendData() {
+      console.log('sending');
+      this.$http.post('https://httpbin.org/post', this.input, {
+        headers: {
+          'content-type': 'application/json',
+        },
+      }).then((result) => {
+        this.response = result.data;
+      }, (error) => {
+        console.error(error);
+      });
+    },
+  },
+};
 
 </script>
--- src/vuejs-jest-example/src/main.js
+++ src/vuejs-jest-example/src/main.js
@@ -4,17 +4,17 @@ import App from './App.vue';
 import router from './router';
 
 Vue.use(VueResource);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    el: '#app',
-    data: {
-        httpbin: 'https://httpbin.org/',
-        attributeA: 'valueA',
-        attributeB: 'valueB'
-    },
-    router,
-    render: h => h(App),
+  el: '#app',
+  data: {
+    httpbin: 'https://httpbin.org/',
+    attributeA: 'valueA',
+    attributeB: 'valueB',
+  },
+  router,
+  render: h => h(App),
 }).$mount('#app');
 
-Vue.prototype.$httpbin = Object.freeze('https://httpbin.org/')
+Vue.prototype.$httpbin = Object.freeze('https://httpbin.org/');
