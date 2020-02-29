09eb8e9cebcb83ae97bf1424bd2c1bdaba2cc2a2
sylhare <sylhare@outlook.com>
2020-02-28 19:04:36 -0500

    Update proxy example

--- src/vuejs-proxy-example/proxy-cors/proxy.js
+++ src/vuejs-proxy-example/proxy-cors/proxy.js
@@ -8,6 +8,6 @@ cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
+}).listen(port, host, function () {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
--- src/vuejs-proxy-example/proxy/index.js
+++ src/vuejs-proxy-example/proxy/index.js
@@ -10,11 +10,11 @@ server.use(express.static(__dirname + '/app'));
 var apiProxy = httpProxy.createProxyServer();
 
 // Grab all requests to the server with "/space/".
-server.all("/space/*", function(req, res) {
+server.all("/space/*", function (req, res) {
     console.log("Request made to /space/");
     apiProxy.web(req, res, {target: apiForwardingUrl});
 });
 
-server.listen(server.get('port'), function() {
+server.listen(server.get('port'), function () {
     console.log('Express server listening on port ' + server.get('port'));
 });
